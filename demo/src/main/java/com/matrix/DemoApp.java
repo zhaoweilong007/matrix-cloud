63177cb397ece5978311d8d9add0a926293b6749
zhaoweilong <zhaoweilong176@gmail.com>
2022-06-30 14:27:46 +0800

    更新项目依赖，增加common组件

--- demo/src/main/java/com/matrix/DemoApp.java
+++ demo/src/main/java/com/matrix/DemoApp.java
@@ -2,6 +2,7 @@ package com.matrix;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * 描述：
@@ -9,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author zwl
  * @since ${DATE} ${TIME}
  **/
+@EnableDiscoveryClient
 @SpringBootApplication
 public class DemoApp {
     public static void main(String[] args) {
